50723e4a8fcf8f6a45db2fad8a9c8edbd7e84268
IgorPotapov <work.igorpotapov@gmail.com>
2021-07-28 17:08:56 +0300

    added cors

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import getItems from './routes/items/items.get.js';
 import postItem from './routes/items/item.post.js';
 import patchItem from './routes/items/item.patch.js';
 import deleteItem from './routes/items/item.delete.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
