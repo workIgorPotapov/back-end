a7196b886f4007a5434c05e92d0e57460f0293e2
IgorPotapov <work.igorpotapov@gmail.com>
2021-07-29 11:06:19 +0300

    changed cors

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
